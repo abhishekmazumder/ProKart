1602d98ceaa47ea8cea60ad383e401fd3e47c36f
Abhishek Mazumder <abhishekmazumder69@gmail.com>
2022-10-17 23:56:21 +0530

    Single ProductScreen layout implemented

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,9 +1,74 @@
-import React from 'react'
+import React from 'react';
+import { Link } from "react-router-dom";
+import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
+import Rating from "../components/Rating";
+import products from "../products";
 
-const ProductScreen = () => {
+const ProductScreen = ({ match }) => {
+  // const {id}= match.params;
+  // const product = products.find(p => p._id === match.params.id);
+  // console.log(id)
+  const product = {
+    _id: '5',
+    name: 'Logitech G-Series Gaming Mouse',
+    image: '/images/mouse.jpg',
+    description:
+      'Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience',
+    brand: 'Logitech',
+    category: 'Electronics',
+    price: 49.99,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  };
+
   return (
-    <div>ProductScreen</div>
-  )
-}
+    <>
+      <Link className='btn btn-dark my-3'>Go Back</Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+        <Col md={3}>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h2>{product.name}</h2>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating value={product.rating} text={`${product.numReviews} reviews`} />
+            </ListGroup.Item>
+            <ListGroup.Item>
+              Price: ${product.price}
+            </ListGroup.Item>
+            <ListGroup.Item>
+              {product.description}
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col><strong>${product.price}</strong></Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col><strong>{product.countInStock > 0 ? "In Stock" : "Out Of Stock"}</strong></Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Button className='btn-block' type="Button" disabled={product.countInStock === 0}>Add To Cart</Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
+};
 
-export default ProductScreen
+export default ProductScreen;
