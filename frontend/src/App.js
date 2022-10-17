2da1f5b6a5c7216656e53e310aee4acba0bea5b5
Abhishek Mazumder <abhishekmazumder69@gmail.com>
2022-10-17 08:45:10 -0700

    HomeScreen of Product List and React-Routing added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,25 @@
 import { Container } from "react-bootstrap";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 const App = () => {
   return (
     <>
-      <Header />
-      <main className="py-3">
-        <Container>
-          <h1>ProKart</h1>
-        </Container>
-      </main>
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <main className="py-3">
+          <Container>
+            <Routes>
+              <Route exact path="/" element={<HomeScreen />} />
+              <Route path="/product/:id" element={<ProductScreen />} />
+            </Routes>
+          </Container>
+        </main>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 };
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { LinkContainer } from "react-router-bootstrap";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -9,18 +10,22 @@ const Header = () => {
     <header>
       <Navbar bg="dark" variant="dark" expand="lg">
         <Container>
-          <Navbar.Brand href="/">ProKart</Navbar.Brand>
+          <LinkContainer to="/"><Navbar.Brand>ProKart</Navbar.Brand></LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
-              <Nav.Link href="/cart"><i className='fas fa-shopping-cart'></i> Cart</Nav.Link>
-              <Nav.Link href="/login"><i className='fas fa-user'></i> Sign In</Nav.Link>
+              <LinkContainer to="/cart">
+                <Nav.Link><i className='fas fa-shopping-cart'></i> Cart</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link><i className='fas fa-user'></i> Sign In</Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
